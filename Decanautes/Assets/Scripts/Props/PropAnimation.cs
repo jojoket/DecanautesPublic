51b19591f847a6b4eab53810bb259d65710fde4d
jojoket <jjonchay12@gmail.com>
2024-05-24 16:02:27 +0200

    fix : prop animations merge error

--- Decanautes/Assets/Scripts/Props/PropAnimation.cs
+++ Decanautes/Assets/Scripts/Props/PropAnimation.cs
@@ -11,13 +11,13 @@ public class PropAnimation : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     [Button]
