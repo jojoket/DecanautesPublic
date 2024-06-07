d07ef73ed85ed9e1ac8c9fbc5557822a5db22731
Alvina <alvinadr24@gmail.com>
2024-06-07 11:49:06 +0200

    feat: input icons

--- Decanautes/Assets/Samples/Input System/1.7.0/Rebinding UI/GamepadIconsExample.cs
+++ Decanautes/Assets/Samples/Input System/1.7.0/Rebinding UI/GamepadIconsExample.cs
@@ -1,6 +1,5 @@
 using System;
 using UnityEngine.UI;
-
 ////TODO: have updateBindingUIEvent receive a control path string, too (in addition to the device layout name)
 
 namespace UnityEngine.InputSystem.Samples.RebindUI
@@ -13,8 +12,7 @@ namespace UnityEngine.InputSystem.Samples.RebindUI
     /// </summary>
     public class GamepadIconsExample : MonoBehaviour
     {
-        public GamepadIcons xbox;
-        public GamepadIcons ps4;
+        public ControlerIcons Icons;
 
         protected void OnEnable()
         {
@@ -34,9 +32,10 @@ namespace UnityEngine.InputSystem.Samples.RebindUI
 
             var icon = default(Sprite);
             if (InputSystem.IsFirstLayoutBasedOnSecond(deviceLayoutName, "DualShockGamepad"))
-                icon = ps4.GetSprite(controlPath);
+                icon = Icons.ps4.GetSprite(controlPath);
             else if (InputSystem.IsFirstLayoutBasedOnSecond(deviceLayoutName, "Gamepad"))
-                icon = xbox.GetSprite(controlPath);
+                icon = Icons.xbox.GetSprite(controlPath);
+            else icon = Icons.computer.GetSprite(controlPath);
 
             var textComponent = component.bindingText;
 
@@ -54,59 +53,6 @@ namespace UnityEngine.InputSystem.Samples.RebindUI
             {
                 textComponent.gameObject.SetActive(true);
                 imageComponent.gameObject.SetActive(false);
-            }
-        }
-
-        [Serializable]
-        public struct GamepadIcons
-        {
-            public Sprite buttonSouth;
-            public Sprite buttonNorth;
-            public Sprite buttonEast;
-            public Sprite buttonWest;
-            public Sprite startButton;
-            public Sprite selectButton;
-            public Sprite leftTrigger;
-            public Sprite rightTrigger;
-            public Sprite leftShoulder;
-            public Sprite rightShoulder;
-            public Sprite dpad;
-            public Sprite dpadUp;
-            public Sprite dpadDown;
-            public Sprite dpadLeft;
-            public Sprite dpadRight;
-            public Sprite leftStick;
-            public Sprite rightStick;
-            public Sprite leftStickPress;
-            public Sprite rightStickPress;
-
-            public Sprite GetSprite(string controlPath)
-            {
-                // From the input system, we get the path of the control on device. So we can just
-                // map from that to the sprites we have for gamepads.
-                switch (controlPath)
-                {
-                    case "buttonSouth": return buttonSouth;
-                    case "buttonNorth": return buttonNorth;
-                    case "buttonEast": return buttonEast;
-                    case "buttonWest": return buttonWest;
-                    case "start": return startButton;
-                    case "select": return selectButton;
-                    case "leftTrigger": return leftTrigger;
-                    case "rightTrigger": return rightTrigger;
-                    case "leftShoulder": return leftShoulder;
-                    case "rightShoulder": return rightShoulder;
-                    case "dpad": return dpad;
-                    case "dpad/up": return dpadUp;
-                    case "dpad/down": return dpadDown;
-                    case "dpad/left": return dpadLeft;
-                    case "dpad/right": return dpadRight;
-                    case "leftStick": return leftStick;
-                    case "rightStick": return rightStick;
-                    case "leftStickPress": return leftStickPress;
-                    case "rightStickPress": return rightStickPress;
-                }
-                return null;
             }
         }
     }
