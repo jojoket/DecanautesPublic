8666c2db59061b08f0d4612c010a429e64999279
jojoket <60076538+jojoket@users.noreply.github.com>
2024-06-11 12:18:54 +0200

    build 1 with bug fixes

--- Decanautes/Assets/Scripts/Save/PlayerPreferencesManager.cs
+++ Decanautes/Assets/Scripts/Save/PlayerPreferencesManager.cs
@@ -14,10 +14,29 @@ public class PlayerPreferencesManager : MonoBehaviour
 
     public Material Quadrichromic;
 
+    private FMOD.Studio.VCA MusicVcaController;
+    public string MusicVcaName;
+    private FMOD.Studio.VCA AmbianceVcaController;
+    public string AmbianceVcaName;
+    private FMOD.Studio.VCA VFXVcaController;
+    public string VFXVcaName;
+    private FMOD.Studio.VCA UIVcaController;
+    public string UIVcaName;
+
+
+
     void Awake()
     {
         Instance = this;
         ApplyPreferences();
+    }
+
+    private void Start()
+    {
+        MusicVcaController = FMODUnity.RuntimeManager.GetVCA("vca:/" + MusicVcaName);
+        AmbianceVcaController = FMODUnity.RuntimeManager.GetVCA("vca:/" + AmbianceVcaName);
+        VFXVcaController = FMODUnity.RuntimeManager.GetVCA("vca:/" + VFXVcaName);
+        UIVcaController = FMODUnity.RuntimeManager.GetVCA("vca:/" + UIVcaName);
     }
 
     public void ApplyPreferences()
@@ -37,8 +56,11 @@ public class PlayerPreferencesManager : MonoBehaviour
         Quadrichromic.SetFloat("Opacity", PlayerPreferencesData.QuadrichromicMode ? 1 : 0);
 
         //Sounds
-
-    }
+        MusicVcaController.setVolume(PlayerPreferencesData.MusicVolume);
+        AmbianceVcaController.setVolume(PlayerPreferencesData.AmbianceVolume);
+        VFXVcaController.setVolume(PlayerPreferencesData.VFXVolume);
+        UIVcaController.setVolume(PlayerPreferencesData.UIVolume);
+    }    
 
     public void ChangeSensibility(float newSensibility)
     {
